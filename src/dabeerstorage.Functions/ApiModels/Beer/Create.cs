c90844dcfa8206fe46356277512a92de90e77828
Brian Sheridan <brian.sheridan@gmail.com>
2020-03-07 11:37:55 -0800

    Fixed Models

--- src/dabeerstorage.Functions/ApiModels/Beer/Create.cs
+++ src/dabeerstorage.Functions/ApiModels/Beer/Create.cs
@@ -1,7 +1,12 @@
 namespace DaBeerStorage.Functions.ApiModels.Beer
 {
-    public class Create
+    public class Create : Models.Beer
     {
-        
+        public Create()
+        {
+            Quantity = "1";
+        }
+        public string Quantity { get; set; }
+        public string UserName { get; set; }
     }
 }
---
+++ src/dabeerstorage.Functions/Untappd/UntappdRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DaBeerStorage.Functions.Config;
+using DaBeerStorage.Functions.Interfaces;
+using DaBeerStorage.Functions.Untappd.Models.BeerSearch;
+using Refit;
+using Beer = DaBeerStorage.Functions.Untappd.Models.BeerInfo.Beer;
+
+namespace DaBeerStorage.Functions.Untappd
+{
+    public class UntappdRepository : IBeerSearchRepository
+    {
+        private readonly UntappdApiOptions _options;
+        private readonly IUntappdApi _client;
+
+        public UntappdRepository(UntappdApiOptions options)
+        {
+            _options = options;
+            _client = RestService.For<IUntappdApi>(_options.Url);
+        }
+        public async Task<IList<Item>> SearchByName(int page, string beerName)
+        {
+            var response = await _client.GetBeerSearchAsync(_options.ClientId, _options.ClientKey, beerName);
+
+            return response.Response.Beers.Items;
+
+        }
+
+        public async Task<Beer> SearchById(int beerId)
+        {
+            var response = await _client.GetBeerById(_options.ClientId, _options.ClientKey, beerId);
+            return response;
+        }
+    }
+}

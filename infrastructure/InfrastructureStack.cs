bdfc1e14a284c268baf4e19e5ac2952173e1a9d6
Brian Sheridan <brian.sheridan@gmail.com>
2020-03-06 21:30:24 -0800

    added api gateway and broke out functions

--- infrastructure/InfrastructureStack.cs
+++ infrastructure/InfrastructureStack.cs
@@ -1,5 +1,7 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.APIGateway;
 using Amazon.CDK.AWS.Lambda;
+using Resource = Amazon.CDK.AWS.APIGateway.Resource;
 
 namespace dabeerstorage.Infrastructure
 {
@@ -7,16 +9,41 @@ namespace dabeerstorage.Infrastructure
     {
         internal InfrastructureStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-            CreateFunction("CreateBeer");
-           
+            var createbeerfunc = CreateFunction("CreateBeer");
+
+            var api = CreateAPI("DaBeerStorage");
+        
+            var beerResource = CreateResource("beer",api);
+            
+            AddMethod("Post",createbeerfunc,beerResource);
+            
         }
 
-        private void CreateFunction(string methodName)
+        private RestApi CreateAPI(string name)
+        {
+            return new RestApi(this, name.ToLower(), new RestApiProps()
+            {
+                RestApiName = name
+            });
+        }
+
+        private Resource CreateResource(string path,RestApi api)
+        {
+            return api.Root.AddResource(path);
+        }
+
+        private void AddMethod(string httpMethod, Function function, Resource apiGatewayResource)
+        {
+            var integration = new LambdaIntegration(function);
+            apiGatewayResource.AddMethod(httpMethod, integration);
+        }
+        
+        private Function CreateFunction(string methodName)
         {
             var pathToPublishFolder = "../src/dabeerstorage.Functions/bin/Release/netcoreapp2.1/publish";
             var functionClass = "dabeerstorage.Functions::dabeerstorage.Functions.Function::";
             
-            var createbeer = new Function(this,methodName.ToLower(), new FunctionProps() {
+            return new Function(this,methodName.ToLower(), new FunctionProps() {
                 Runtime = Runtime.DOTNET_CORE_2_1,
                 FunctionName = $"DaBeerStorage_{methodName}",
                 Timeout = Duration.Minutes(1),

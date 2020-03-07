cfd34b0a52776c598b4f2d7d4d666e6ad01ff017
Brian Sheridan <brian.sheridan@gmail.com>
2020-03-06 20:37:36 -0800

    Created Project template

---
+++ infrastructure/Program.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace dabeerstorage.Infrastructure
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new InfrastructureStack(app, "InfrastructureStack");
+            app.Synth();
+        }
+    }
+}
---
+++ src/dabeerstorage.Functions/Config/AppConfig.cs
@@ -0,0 +1,7 @@
+namespace dabeerstorage.Functions
+{
+    public class AppConfig
+    {
+        
+    }
+}
